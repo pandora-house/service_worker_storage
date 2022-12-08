2e5521ad6751c7a58992ba0e74745a3728cbf485
Dima <pandora-house@mail.ru>
2022-12-08 11:40:36 +0300

    add src

--- lib/service_worker_storage.dart
+++ lib/service_worker_storage.dart
@@ -1,4 +1,4 @@
 library service_worker_storage;
 
-export 'service_worker_storage.io.dart'
-    if (dart.library.html) 'service_worker_storage.web.dart';
+export 'src/service_worker_storage.io.dart'
+    if (dart.library.html) 'src/service_worker_storage.web.dart';
